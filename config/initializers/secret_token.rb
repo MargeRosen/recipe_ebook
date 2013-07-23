1dd7fec9904f8b364c62e665db1f32b4e4d2b02d
Marge Rosen <marge@masterclassu.com>
2013-07-23 12:48:22 -0700

    Add app stub

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RecipeEbook::Application.config.secret_key_base = 'eff6bcf4a8f6fa334034937013a63491cf77572d997349d894bf5741fbd22977622b33132a9426491d3a0bdbf37a9418f706978973ebfaed2db992b5c2ec2a63'
